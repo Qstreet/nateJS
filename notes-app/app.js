da1be5cc8645f83b19cb987916514d2d67fe1004
Qstreet <git@qstreet.org>
2019-09-07 04:40:10 -0400

    Edits

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,5 @@
 const chalk = require('chalk')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 const yargs = require('yargs')
 
 yargs.version('1.1.0')
@@ -20,8 +20,7 @@ yargs.command({
     }
   },
   handler: function (argv) {
-    console.log('Title: ' + argv.title)
-    console.log('Body: ' + argv.body)
+   notes.addNote(argv.title, argv.body)
   }
 })
 
