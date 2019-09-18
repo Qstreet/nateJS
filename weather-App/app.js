c13c7200b3b5876a67f8a9b5e4ba5e87bb485161
Qstreet <git@qstreet.org>
2019-09-18 03:51:51 -0400

    Update

--- weather-App/app.js
+++ weather-App/app.js
@@ -1,11 +1,8 @@
-console.log('Starting');
+const request =  require('request')
 
-setTimeout(() => {
-  console.log('2 Second Timer');
-}, 2000)
+const url = 'https://api.darksky.net/forecast/6a4f86c199afab05342e0438006efab0/37.8267,-122.4233?units=si'
 
-setTimeout(() => {
-  console.log('0 Second Timer');
-}, 0)
+request({ url: url, json: true }, (error, response) => {
 
-console.log('Stopping');
+  console.log(`${response.body.daily.data[0].summary} the chance of rain is ${response.body.currently.precipProbability}%, and it is ${response.body.currently.apparentTemperature} degrees`);
+})
