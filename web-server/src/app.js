b9ac6f94eb8c97cf5e44eddee96a0860ae0df81e
Qstreet <git@qstreet.org>
2019-10-09 04:39:18 -0400

    Nate

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,29 +1,44 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDir = path.join(__dirname, '../public')
 
+// Define Paths for Express Config
+const publicDir = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join (__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to server
 app.use(express.static(publicDir))
 
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather App',
-    name: 'Nate'
+    name: 'Nate',
+    footer: "Created by Nate"
   })
 })
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'about me',
-    name: 'Nae'
+    name: 'Nae',
+    footer: "Created by Nate"
   })
 })
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    whatDo: 'Enter localhost:3000/about into your url.'
+    whatDo: 'Enter localhost:3000/about into your url.',
+    title: 'Help',
+    name: 'Nate',
+    footer: "Created by Nate"
   })
 })
 
