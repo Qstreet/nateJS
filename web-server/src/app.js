17920747f00b7ded5e9570fe81104caa13c24e48
Qstreet <git@qstreet.org>
2019-10-22 04:18:43 -0400

    Wx

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,9 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
 const app = express()
 
 // Define Paths for Express Config
@@ -46,11 +49,23 @@ app.get('/weather', (req, res) => {
       error: 'You must provide an address'
     })
   }
-  console.log(req.query.address)
-  res.send({
-    forecast: 'It is snowing',
-    location: 'DC',
-    address: req.query.address
+
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error })
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+       if (error) {
+        return res.send({ error })
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        adress: req.query.address
+      })
+    })
   })
 })
 
